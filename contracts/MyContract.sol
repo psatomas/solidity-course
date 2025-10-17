fd176d48dc8adffcae33aa199ced43cb8642f3eb
psatomas <psatomas@gmail.com>
2025-10-17 16:33:16 -0300

    first commit

---
+++ contracts/MyContract.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.30;
+
+contract MyContract {
+    
+    string public ourstring = "Hello World";
+
+    function updateOurString(string memory _updateString) public {
+        ourstring = _updateString;
+    }
+}
