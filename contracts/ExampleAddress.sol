5641ec8b2e248b31da67170dce2a74295621d5ae
psatomas <psatomas@gmail.com>
2025-10-22 19:09:21 -0300

    feat: demonstrate address type usage and balance check

---
+++ contracts/ExampleAddress.sol
@@ -0,0 +1,16 @@
+//Spdx-License-Identifier: MIT
+
+pragma solidity ^0.8.28;
+
+contract ExampleAddress {
+
+    address public someAddress;
+
+    function setSomeAddress(address _someAddress) public {
+        someAddress = _someAddress;
+    }
+
+    function getAddressBalance() public view returns (uint) {
+        return someAddress.balance;
+    }
+}
