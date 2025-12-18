2b3795503a142a5fb4d2662c4c24e5bbda87beb8
psatomas <psatomas@gmail.com>
2025-12-18 12:41:25 -0300

    feat(ErrorHandling): add try/catch example with custom errors and event logging

---
+++ contracts/ExampleTryCatch.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.28;
+
+contract WillThrow {
+    error NotAllowedError(string);
+    function aFunction() public pure {
+        revert NotAllowerdError("You are not allowed");
+    }
+}
+
+contract ErrorHandling {
+    event ErrorLogging(string reason);
+    event ErrorLogCode(uint256 code);
+    event ErrorLogBytes(bytes lowLevelData);
+    function catchTheError() public {
+        WillThrow will = new WillThrow();
+        try will.aFunction() {
+            // do something
+        } catch Error(string memory reason) {
+            emit ErrorLogging(reason);
+        } catch Panic(uint256 errorCode) {
+            emit ErrorLogCode(errorCode);
+        } catch (bytes memory lowLevelData) {
+            emit ErrorLogBytes(lowLevelData);
+        }
+    }
+}
