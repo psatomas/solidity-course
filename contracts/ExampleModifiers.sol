5674aebeee00308da9e36bb11fb836d4ec0840ff
psatomas <psatomas@gmail.com>
2026-01-02 19:37:09 -0300

    feat: add token contract with ownership modifier

---
+++ contracts/ExampleModifiers.sol
@@ -0,0 +1,39 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.28;
+
+contract InheritanceModifierExample {
+
+    mapping(address => uint256) public tokenBalance;
+    
+    address owner;
+    
+    uint256 tokenPrice = 1 ether;
+
+    constructor() {
+        owner = msg.sender;
+        tokenBalance[msg.sender] = 100;
+    }
+
+    function createNewToken() public {
+        require(msg.sender == owner, "You are not allowed");
+        tokenBalance[owner]++;
+    }
+
+    function burnToken() public {
+        require(msg.sender == owner, "You are not allowed");
+        tokenBalance[owner]--;
+    }
+
+    function purchaseToken() public payble {
+        require((tokenBalance[owner] * tokenPrice) / msg.value > 0, "Not enough tokens");
+        tokenBalance[owner] -= msg.value / tokenPrice;
+        tokenBalance[msg.sender] += msg.value / tokenPrice;
+    }
+
+    function sendToken(address _to, uint256 _amount) public {
+        require(tokenBalance[msg.sender] >= _amount, "Not enough tokens");
+        tokenBalance[msg.sender] -= _amount;
+        tokenBalance[_to] += _amount;
+    }
+}
